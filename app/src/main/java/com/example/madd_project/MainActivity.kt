7a4d9ca1d3dbf791fcab083e90ffe11cd74702dc
IT21265242Zimmendra <99347087+IT21265242Zimmendra@users.noreply.github.com>
2023-05-05 02:37:49 +0530

    CREATE IS DONE, due to some errors couldn't run it

--- app/src/main/java/com/example/madd_project/MainActivity.kt
+++ app/src/main/java/com/example/madd_project/MainActivity.kt
@@ -6,10 +6,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
 import android.widget.Button
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.madd_project.databinding.ActivityMainBinding
 import com.google.firebase.FirebaseApp
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -24,6 +29,12 @@ class MainActivity : AppCompatActivity() {
 //                )
 //            )
 //        }
+
+
+        setContentView(binding.root)
+        val navController = findNavController(R.id.fragmentContainerView)
+        val appBarConfiguration= AppBarConfiguration(setOf(R.id.postHomeFragment,R.id.Create_Post))
+        setupActionBarWithNavController(navController,appBarConfiguration)
     }
 
     // this event will enable the back
@@ -37,5 +48,7 @@ class MainActivity : AppCompatActivity() {
         }
         return super.onContextItemSelected(item)
     }
+
+
 
 }
