34fea2dbd3cdeb0f1293383d6164e7c57d6cbd12
IT21265242Zimmendra <99347087+IT21265242Zimmendra@users.noreply.github.com>
2023-05-05 23:00:30 +0530

    Crud IS DONE, due to some errors couldn't run it

--- app/src/main/java/com/example/madd_project/models/Posts.kt
+++ app/src/main/java/com/example/madd_project/models/Posts.kt
@@ -1,10 +1,11 @@
 package com.example.madd_project.models
 
-data class Posts(val id :String?=null,
-                 val name:String?=null,
-                 val dueDate:String?=null,
-                 val description:String?=null,
-                 val quantity:String?=null,
-                 val imageUrl:String?=null
+data class Posts(
+    val id: String =null,
+    val name:String?=null,
+    val dueDate:String?=null,
+    val description:String?=null,
+    val quantity:String?=null,
+    val imageUrl:String?=null
 
 )
