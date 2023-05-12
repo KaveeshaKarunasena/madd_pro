4b11f7590aee7fed9f79c8df5731d1904cc36fd8
KaveeshaKarunasena <kaveeshalankeshwara2001@gmail.com>
2023-05-12 16:44:06 +0530

    home frag

--- app/src/main/java/com/example/madd_project/MainActivity.kt
+++ app/src/main/java/com/example/madd_project/MainActivity.kt
@@ -10,15 +10,26 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.madd_project.databinding.ActivityMainBinding
+import com.example.madd_project.fragments.HomeFragment
 import com.google.firebase.FirebaseApp
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         FirebaseApp.initializeApp(this);
+        val fragmentHome = HomeFragment()
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.container, fragmentHome)
+            commit()
+
+            println("maint activity")
+
+
 //        val button: Button = findViewById(R.id.backBtn)
 
 //        button.setOnClickListener { view ->
@@ -31,21 +42,20 @@ class MainActivity : AppCompatActivity() {
 //        }
 
 
-
-    }
-
-    // this event will enable the back
-    // function to the button on press
-    override fun onContextItemSelected(item: MenuItem): Boolean {
-        when (item.itemId) {
-            android.R.id.home -> {
-                finish()
-                return true
-            }
         }
-        return super.onContextItemSelected(item)
+
+        // this event will enable the back
+        // function to the button on press
+//    override fun onContextItemSelected(item: MenuItem): Boolean {
+//        when (item.itemId) {
+//            android.R.id.home -> {
+//                finish()
+//                return true
+//            }
+//        }
+//        return super.onContextItemSelected(item)
+//    }
+
     }
-
-
 
 }
