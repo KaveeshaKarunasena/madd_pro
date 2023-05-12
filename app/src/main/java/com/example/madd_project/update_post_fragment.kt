c88996d2e6b6074e381461e0f6aa5b9820d3fe7f
Dinusha-hue <dinushaweerasekara312@gmail.com>
2023-05-12 19:19:13 +0530

    create the post side

--- app/src/main/java/com/example/madd_project/update_post_fragment.kt
+++ app/src/main/java/com/example/madd_project/update_post_fragment.kt
@@ -8,9 +8,10 @@ import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
-import com.example.models.Posts
-import com.example.newapp.R
-import com.example.newapp.databinding.FragmentUpdateBinding
+import com.example.fragments.update_post_fragmentArgs
+import com.example.madd_project.models.Posts
+import com.example.madd_project.R
+import com.example.madd_project.databinding.FragmentUpdatePostFragmentBinding
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 
@@ -20,7 +21,7 @@ import com.google.firebase.database.FirebaseDatabase
  * create an instance of this fragment.
  */
 class update_post_fragment : Fragment() {
-    private var _binding: FragmentUpdateBinding?=null
+    private var _binding: FragmentUpdatePostFragmentBinding?=null
     private val binding get() = _binding!!
 
     private val args : update_post_fragmentArgs by navArgs()
@@ -31,43 +32,45 @@ class update_post_fragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        _binding = FragmentUpdateBinding.inflate(inflater,container,false)
-        binding.floatingActionButton.setOnClickListener{
-            findNavController().navigate(R.id.action_update_Fragment_to_homeFragment)
-        }
+        _binding = FragmentUpdatePostFragmentBinding.inflate(inflater,container,false)
+//        binding.floatingActionButton.setOnClickListener{
+//            findNavController().navigate(R.id.action_update_Fragment_to_homeFragment)
+//        }
         firebaseRef = FirebaseDatabase.getInstance().getReference("Posts")
 
         binding.apply {
-            Name.setText(args.name)
+            name.setText(args.name)
             dueDate.setText(args.dueDate)
-            Description.setText(args.description)
+            description.setText(args.description)
             quantity.setText(args.quantity)
 
-            UpdatePost.setOnClickListener{
+
+            updatePost.setOnClickListener{
                 updateData()
+                findNavController().navigate(R.id.action_update_Fragment_to_homeFragment)
             }
         }
         return binding.root
     }
 
     private fun updateData() {
-        val name = binding.Name.text.toString()
+        val name = binding.name.text.toString()
         val duedate = binding.dueDate.text.toString()
-        val description = binding.Description.text.toString()
+        val description = binding.description.text.toString()
         val quantity = binding.quantity.text.toString()
         val Posts = Posts(args.id,name,duedate,description,quantity)
-         firebaseRef.child(args.id).setValue(Posts)
-             .addOnCompleteListener {
-                 Toast.makeText(
-                     context,
-                     "Post has been updated successfully",
-                     Toast.LENGTH_SHORT
-                 ).show()
-             }
-             .addOnFailureListener {
-                 Toast.makeText(context, "error ${it.message}", Toast.LENGTH_SHORT)
-                     .show()
-             }
+        firebaseRef.child(args.id).setValue(Posts)
+            .addOnCompleteListener {
+                Toast.makeText(
+                    context,
+                    "Post has been updated successfully",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+            .addOnFailureListener {
+                Toast.makeText(context, "error ${it.message}", Toast.LENGTH_SHORT)
+                    .show()
+            }
     }
 
 
