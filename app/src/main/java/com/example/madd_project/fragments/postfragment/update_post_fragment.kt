27b4dd06ef83028ccb6ef5f13ddd6e4d0f0db0cd
Dinusha-hue <dinushaweerasekara312@gmail.com>
2023-05-12 15:06:39 +0530

    editr

--- app/src/main/java/com/example/madd_project/fragments/postfragment/update_post_fragment.kt
+++ app/src/main/java/com/example/madd_project/fragments/postfragment/update_post_fragment.kt
@@ -1,71 +1,75 @@
-//package com.example.madd_project.fragments.postfragment
-//
-//import android.os.Bundle
-//import android.view.LayoutInflater
-//import android.view.View
-//import android.view.ViewGroup
-//import android.widget.Toast
-//import androidx.fragment.app.Fragment
-//import androidx.navigation.fragment.navArgs
-//import com.example.madd_project.databinding.FragmentUpdatePostFragmentBinding
-//import com.example.madd_project.models.Posts
-//import com.google.firebase.database.DatabaseReference
-//import com.google.firebase.database.FirebaseDatabase
-//
-///**
-// * A simple [Fragment] subclass.
-// * Use the [update_post_fragment.newInstance] factory method to
-// * create an instance of this fragment.
-// */
-//class update_post_fragment : Fragment() {
-//
-//    private var _binding:FragmentUpdatePostFragmentBinding?=null
-//    private val binding get() = _binding!!
-//    //private val args: update_post_fragmentArgs by navArgs()
-//
-//    private lateinit var firebaseRef: DatabaseReference
-//    override fun onCreateView(
-//        inflater: LayoutInflater, container: ViewGroup?,
-//        savedInstanceState: Bundle?
-//    ): View? {
-//        // Inflate the layout for this fragment
-//        _binding = FragmentUpdatePostFragmentBinding.inflate(inflater,container,false)
-//        firebaseRef = FirebaseDatabase.getInstance().getReference("posts")
-//        binding.apply {
-//            updateName.setText(args.name)
-//            updateDescription.setText(args.description)
-//            updatedueDate.setText(args.dueDate)
-//            updatequantity.setText(args.quantity)
-//            updatePost.setOnClickListener{
-//                updateData()
-//            }
-//        }
-//
-//        return binding.root
-//    }
-//
-//    private fun updateData() {
-//        val name = binding.updateName.text.toString()
-//        val descritpion = binding.updateDescription.text.toString()
-//        val dueDate = binding.updatedueDate.text.toString()
-//        val quantity = binding.updatequantity.text.toString()
-//        val posts = Posts(args.id,name,dueDate,descritpion,quantity)
-//
-//        firebaseRef.child(args.id).setValue(posts)
-//            .addOnCompleteListener {
-//                Toast.makeText(
-//                    context,
-//                    "Post has been updated successfully",
-//                    Toast.LENGTH_SHORT
-//                ).show()
-//            }
-//            .addOnFailureListener {
-//                Toast.makeText(context, "error ${it.message}", Toast.LENGTH_SHORT)
-//                    .show()
-//            }
-//    }
-//
-//
-//}
-//
-//
+package com.example.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
+import com.example.models.Posts
+import com.example.newapp.R
+import com.example.newapp.databinding.FragmentUpdateBinding
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [update_post_fragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class update_post_fragment : Fragment() {
+    private var _binding: FragmentUpdateBinding?=null
+    private val binding get() = _binding!!
+
+    private val args : update_post_fragmentArgs by navArgs()
+
+    private lateinit var firebaseRef: DatabaseReference
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        _binding = FragmentUpdateBinding.inflate(inflater,container,false)
+        binding.floatingActionButton.setOnClickListener{
+            findNavController().navigate(R.id.action_update_Fragment_to_homeFragment)
+        }
+        firebaseRef = FirebaseDatabase.getInstance().getReference("Posts")
+
+        binding.apply {
+            Name.setText(args.name)
+            dueDate.setText(args.dueDate)
+            Description.setText(args.description)
+            quantity.setText(args.quantity)
+
+            UpdatePost.setOnClickListener{
+                updateData()
+            }
+        }
+        return binding.root
+    }
+
+    private fun updateData() {
+        val name = binding.Name.text.toString()
+        val duedate = binding.dueDate.text.toString()
+        val description = binding.Description.text.toString()
+        val quantity = binding.quantity.text.toString()
+        val Posts = Posts(args.id,name,duedate,description,quantity)
+         firebaseRef.child(args.id).setValue(Posts)
+             .addOnCompleteListener {
+                 Toast.makeText(
+                     context,
+                     "Post has been updated successfully",
+                     Toast.LENGTH_SHORT
+                 ).show()
+             }
+             .addOnFailureListener {
+                 Toast.makeText(context, "error ${it.message}", Toast.LENGTH_SHORT)
+                     .show()
+             }
+    }
+
+
+}
+
---
+++ app/src/main/java/com/example/madd_project/utils/adapter/rvPostAdapter.kt
@@ -0,0 +1,81 @@
+package com.example.madd_project.utils.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.Navigation.findNavController
+import androidx.recyclerview.widget.RecyclerView
+import com.example.fragments.PostHomeFragmentDirections
+
+
+import com.example.madd_project.databinding.RvPostsBinding
+import com.example.madd_project.models.Posts
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.storage.FirebaseStorage
+
+class rvPostAdapter(private val postList: java.util.ArrayList<Posts>) : RecyclerView.Adapter< rvPostAdapter.ViewHolder>(){
+    class ViewHolder(val binding: RvPostsBinding) : RecyclerView.ViewHolder(binding.root){
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+
+       return ViewHolder(RvPostsBinding.inflate(LayoutInflater.from(parent.context),parent,false))
+    }
+
+    override fun getItemCount(): Int {
+        return postList.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val currentItem = postList[position]
+        holder.apply {
+            binding.apply {
+                tvNameItem.text = currentItem.name
+                tvDueDate.text = currentItem.dueDate
+                tvdescription.text = currentItem.description
+                tvQuantity.text = currentItem.quantity
+
+                rvDesc.setOnClickListener{
+                   val action = PostHomeFragmentDirections.actionHomeFragmentToUpdateFragment(
+                        currentItem.id.toString(),
+                       currentItem.name.toString(),
+                       currentItem.dueDate.toString(),
+                       currentItem.description.toString(),
+                       currentItem.quantity.toString(),
+                       currentItem.imageUrl.toString()
+                    )
+                    findNavController(holder.itemView).navigate(action)
+                }
+                deletebtn.setOnClickListener{
+                    MaterialAlertDialogBuilder(holder.itemView.context)
+                        .setTitle("post permanently")
+                        .setMessage("Are you sure you want to delete this item")
+                        .setPositiveButton("Yes"){
+                                _,_-> val firebaseRef = FirebaseDatabase.getInstance().getReference("Posts")
+                                      val storageRef = FirebaseStorage.getInstance().getReference("Images")
+                            storageRef.child(currentItem.id.toString()).delete()
+
+                            currentItem.id?.let { it ->
+                                println("id : $it")
+                                firebaseRef.child(it).removeValue()
+                                    .addOnSuccessListener {
+                                        Toast.makeText(holder.itemView.context,"Item removed successfully",
+                                            Toast.LENGTH_SHORT).show()
+                                    }
+                                    .addOnFailureListener{error ->
+                                        Toast.makeText(holder.itemView.context,"error ${error.message}",
+                                            Toast.LENGTH_SHORT).show()
+                                    }
+                            }
+                        }
+                        .setNegativeButton("no"){
+                                _,_-> Toast.makeText(holder.itemView.context,"cancelled", Toast.LENGTH_SHORT).show()
+                        }
+                        .show()
+                }
+            }
+        }
+    }
+}
